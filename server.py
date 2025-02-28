38d474fced8fefe510dc65816a5404e4400e6864
markbosire <markbosirekenyariri@gmail.com>
2025-02-28 18:18:58 +0300

    test commit 19

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins 1!"
+    return "I should be over all the butterflieeeeeees but i am into you!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
