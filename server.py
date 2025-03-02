a3f4efc4cb47ad38315916dc68ebfafe142484ce
markbosire <markbosirekenyariri@gmail.com>
2025-03-02 15:16:08 +0300

    jenkins template

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return " i am so into you into youuuuu intoo youuuu!!!"
+    return " Fambuloussssssssssss!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
